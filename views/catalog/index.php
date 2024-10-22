a94e3cc1ec35febd1c6e6228d9295965d2b76288
l1mak0 <130081108+l1mak0@users.noreply.github.com>
2024-10-22 16:41:41 +0400

    Update index.php

--- views/catalog/index.php
+++ views/catalog/index.php
@@ -14,7 +14,7 @@ $this->title = 'Каталог';
 <div>
     <div class="page-header">
         <h1>Каталог</h1>
-        <?php if(Yii::$app->user->can('creator')): ?>
+        <?php if(Yii::$app->user->isAdmin(): ?>
             <div class='create-btn'>
                 <svg class='create-button' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 64 64" width="40px" height="40px"><path d="M32,6C17.641,6,6,17.641,6,32s11.641,26,26,26s26-11.641,26-26S46.359,6,32,6z M49,34l-14,1l-1,14h-4l-1-14l-14-1v-4l14-1	l1-14h4l1,14l14,1V34z"/></svg>
                 <ul class='create-ul'>
